94d07431ea70a212add0150711aeeff90cff795d
Samuel Veloso Marçal <samuel.marcal@ligainvest.com.br>
2023-01-09 12:32:49 -0300

    build: added cx oracle to image

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 fission spec init
-fission env create --spec --name onb-us-tax-resd-env --image nexus.sigame.com.br/fission-onboarding-us-tax-residency:0.1.1 --poolsize 0 --version 3 --imagepullsecret "nexus-v3" --spec
+fission env create --spec --name onb-us-tax-resd-env --image nexus.sigame.com.br/fission-onboarding-us-tax-residency:0.1.2 --poolsize 0 --version 3 --imagepullsecret "nexus-v3" --spec
 fission fn create --spec --name onb-us-tax-resd-fn --env onb-us-tax-resd-env --code fission.py --targetcpu 80 --executortype newdeploy --maxscale 3 --requestsperpod 10000 --spec
 fission route create --spec --name onb-us-tax-resd-rt --method PUT --url /onboarding/external_fiscal_tax_confirmation --function onb-us-tax-resd-fn
